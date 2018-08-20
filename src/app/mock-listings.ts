e337af9338dff47c2dbb53cbb70de48a2df87009
Nate Cottle and Ryan Putman <ryan@>
2018-08-20 10:44:13 -0700

    add image links and src tags to display listing image

--- src/app/mock-listings.ts
+++ src/app/mock-listings.ts
@@ -1,12 +1,12 @@
 import { Listing } from './listing.model';
 
 export const LISTINGS: Listing[] = [
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 1, 'thisemail@email.com', 'Right Behind You'),
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 2, 'thisemail@email.com', 'Right Behind You'),
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 3, 'thisemail@email.com', 'Right Behind You'),
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 4, 'thisemail@email.com', 'Right Behind You'),
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 5, 'thisemail@email.com', 'Right Behind You'),
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 6, 'thisemail@email.com', 'Right Behind You'),
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 7, 'thisemail@email.com', 'Right Behind You'),
-  new Listing("Buy My Stuff", 400, 'test-img', 'All of my great stuff', Date.now(), 'Great Stuff', 8, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 1, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 2, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 3, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 4, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 5, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 6, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 7, 'thisemail@email.com', 'Right Behind You'),
+  new Listing("Buy My Stuff", 400, 'puppy.jpg', 'All of my great stuff', Date.now(), 'Great Stuff', 8, 'thisemail@email.com', 'Right Behind You'),
 ];
